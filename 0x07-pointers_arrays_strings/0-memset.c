a6c132c21d78f93f86edffee86a5b11209a21c8c
Herculesisdead <andrew@databit.net>
2023-04-03 12:43:27 -0700

    0X07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * This function will fill a block of memory with a specified value
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}

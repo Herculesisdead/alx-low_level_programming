beaba4a9e232a43310e7ce6da0e44d7e640e4117
Herculesisdead <andrew@databit.net>
2023-04-05 12:05:15 -0700

    this is my commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
- * this is a function that returns 0 
- * however only upon success 
  *
- *
+ * 
  */
 void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursions(s+1);
+		_puts_recursion(s + 1);
 	}
 
 	else
